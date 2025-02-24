cef6ebf3c3fac13401ccdf3cd30e8031f6fd778d
jessl314 <jlchrysanthemum456@gmail.com>
2025-02-24 11:55:31 -0800

    got all dependencies working and made a barebones quote retrieval API route

--- api/src/app.py
+++ api/src/app.py
@@ -32,7 +32,7 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
 
 app = FastAPI(lifespan=lifespan)
 
-
+# endpoint to process a quote submission
 @app.post("/quote")
 def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
     """
@@ -46,5 +46,18 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
     # You may modify the return value as needed to support other functionality
     return RedirectResponse("/", status.HTTP_303_SEE_OTHER)
 
-
+# START EDITING HERE!!---------------------------
 # TODO: add another API route with a query parameter to retrieve quotes based on max age
+
+@app.get("/retrieve", response_model=list[Quote])
+def get_quotes():
+    """
+    API route to obtain quotes from the database.
+    - need to add query parameter to indicate the max age of quotes to return
+    - on the frontend, need a select menu for options to view quotes from last week, month year, or all
+    
+    """
+    # currently: gets quotes in the database under the quote key
+    #  and then returns a list of them or an empty list if there is none
+    quotes_data = database.get("quotes", [])
+    return quotes_data
